07713dd009fd7b8748807bc4fabe33c9d62485d1
kreethimishra12 <88190895+kreethimishra12@users.noreply.github.com>
2021-10-31 22:33:24 +0530

    week 6 updated

---
+++ Week6-Lab-exercise/RecursiveDecentParser.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<string.h>
+int S(),Ldash(),L();
+char *ip;
+char string[50];
+int main()
+{
+ printf("Enter the string\n");
+ scanf("%s",string);
+ ip=string;
+ printf("\n\nInput\tAction\n--------------------------------\n");
+ if(S()){
+ printf("\n--------------------------------\n");
+ printf("\n String is successfully parsed\n");
+ }
+ else{
+ printf("\n--------------------------------\n");
+ printf("Error in parsing String\n");
+ }
+}
+int L()
+{
+ printf("%s\tL->SL' \n",ip);
+ if(S())
+ {
+ if(Ldash())
+ {
+ return 1;
+ }
+ else
+ return 0;
+ }
+ else
+ return 0;
+}
+
+int Ldash()
+{
+ if(*ip==',')
+ {
+ printf("%s\tL'->,SL' \n",ip);
+ ip++;
+ if(S())
+ {
+ if(Ldash())
+ {
+ return 1;
+ }
+ else
+ return 0;
+ }
+ else
+ return 0;
+ }
+ else
+ {
+ printf("%s\tL'->E\n",ip);
+ return 1;
+ }
+}
+
+int S()
+{
+if(*ip == '('){
+ printf("%s\tS->(L) \n",ip);
+ ip++;
+ if(L())
+ {
+ if(*ip ==')')
+ {
+     ip ++;
+ return 1;
+ }
+ else
+ return 0;
+ }
+ else
+ return 0;
+ }
+ else if(*ip == 'a'){
+     ip++;
+     printf("%s\t\tS->a \n",ip);
+     return 1;
+}
+ else
+ return 0;
+}
